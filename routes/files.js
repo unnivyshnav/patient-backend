3b337068662eeacbdbeb12f3b93411f234cf3d7d
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-04-04 16:01:57 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -5,6 +5,7 @@ const Files = require("../models/Files");
 //get
 router.get("/", async (req, res) => {
   try {
+    console.log("hi");
     const file = await Files.find();
     console.log(file);
     res.status(200).json(file);
